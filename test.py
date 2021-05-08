8562f34c78e69fe34a99328720424346b5c2139a
trebuh1996 <82840869+trebuh1996@users.noreply.github.com>
2021-05-08 15:15:02 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # testowe repozytorium
-
+# zmiana która nie pozwoli zrobić push
 # zmiana 
-
+# zmiana która nie pozwoli zrobić push
 # zmiana z github website
+# zmiana która nie pozwoli zrobić push
